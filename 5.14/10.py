b4fbef4c9ab7bbb3e7741bfe22f6a439b5bc39d4
Netti0123 <anettdanko9852@gmail.com>
2022-11-29 08:46:39 +0100

    5.14

---
+++ 5.14/10.py
@@ -0,0 +1,10 @@
+def atfogo():
+    megoldas=0
+    x= 0
+    egy= int(input("Elso befogo"))
+    masodik= int(input("masodik befogo"))
+    x= (egy*egy)+(masodik*masodik)
+    megoldas= x ** 0.5
+
+    print(megoldas)
+atfogo()
