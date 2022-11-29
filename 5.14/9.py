08042728d2abb1c7ff8b6bb3d5809eb385847edd
Netti0123 <anettdanko9852@gmail.com>
2022-11-29 08:32:28 +0100

    5.14

---
+++ 5.14/9.py
@@ -0,0 +1,44 @@
+import turtle
+screen = turtle.Screen()
+t = turtle.Turtle()
+
+def rajzolj_oszlopot(t, magassag):
+    szin(magassag)
+    t.begin_fill()
+    t.pendown()
+    t.left(90)
+    t.forward(magassag)
+    if magassag < 0:
+        t.penup()
+        t.forward(-15)
+        t.write("  "+ str(magassag))
+        t.back(-15)
+        t.pendown()
+    else:
+        t.write("  "+ str(magassag))
+    t.right(90)
+    t.forward(40)
+    t.right(90)
+    t.forward(magassag)
+    t.left(90)
+    t.penup()
+    t.forward(20)
+    t.end_fill()
+def szin(xs):
+    if xs>=200:
+      t.fillcolor("red")
+    if xs>=100 and xs<200:
+      t.fillcolor("yellow")
+    if xs<100:
+      t.fillcolor("green")
+
+ablak = turtle.Screen()
+ablak.bgcolor("lightgreen")
+t.color("blue", "red")
+t.pensize(3)
+t.speed(10)
+xs = [-48,117,200,240,-160,260,-220]
+for m in xs:
+    rajzolj_oszlopot(t, m)
+    szin(m)
+screen.mainloop()
